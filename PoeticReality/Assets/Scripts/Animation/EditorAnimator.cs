40902ddaecd97ad5afb265704612ab649f2f6d45
mikebru <32722658+mikebru@users.noreply.github.com>
2020-07-03 17:49:27 -0500

    working on spirit animation

---
+++ PoeticReality/Assets/Scripts/Animation/EditorAnimator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorAnimator : MonoBehaviour
+{
+
+    public string animationName;
+
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        UpdateAllLayers();
+    }
+
+
+    void UpdateAllLayers()
+    {
+       for(int i=0; i< animator.layerCount; i++)
+        {
+            setAnimationFrame(animationName + (i + 1).ToString(), i, 1);
+        }
+    }
+
+
+    private void setAnimationFrame(string animationName, int layer, float normalizedAnimationTime)
+    {
+        if (animator != null)
+        {
+            animator.speed = 0f;
+            animator.Play(animationName, layer, normalizedAnimationTime);
+            animator.Update(Time.deltaTime);
+        }
+    }
+}
--- PoeticReality/Assets/Scripts/Animation/LimbController.cs
+++ PoeticReality/Assets/Scripts/Animation/LimbController.cs
@@ -7,52 +7,94 @@ public class LimbController : MonoBehaviour
 {
     public string ParameterName;
 
+    [Range(0, 1)]
+    public float LimbExtension;
+
+    [Range(1, 10)]
+    public float LimbPowerFallOff;
+
     public float reachDistance;
 
     private Animator animator;
     private GameObject player;
-    private ChainIKConstraint ChainIK;
+    public ChainIKConstraint ChainIK;
 
     private Vector3 startPosition;
 
+    public float TurbulenceStrength = .1f;
+    public float TurbulenceSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
+
+        startPosition = ChainIK.data.tip.position;
+
+        transform.position = startPosition;
 
         player = Camera.main.gameObject;
         animator = GetComponentInParent<Animator>();
 
-        ChainIK = GetComponent<ChainIKConstraint>();
+        if (ChainIK == null)
+        {
+            ChainIK = GetComponent<ChainIKConstraint>();
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         LimbReach();
+        AddTurbulence();
+    }
+
+    private void OnValidate()
+    {
+       // startPosition = ChainIK.data.tip.position;
+
+        //transform.position = startPosition;
+        //  extendLimb();
     }
 
 
+
+    void extendLimb()
+    {
+        if (animator == null)
+        {
+            animator = GetComponentInParent<Animator>();
+        }
+
+        animator.SetFloat(ParameterName, LimbExtension);
+    }
 
     void LimbReach()
     {
         //distance between limb and player 
-        float distance = Vector3.Distance(player.transform.position, ChainIK.data.tip.position);
+        float distance = Vector3.Distance(player.transform.position, this.transform.position);
 
-        if(distance <= reachDistance)
+        if(distance <= reachDistance + 5)
         {
             float reachLerp = Mathf.Lerp(1, 0, distance / reachDistance);
 
             //reachLerp = Mathf.Pow(reachLerp, 5);
 
-            animator.SetFloat(ParameterName, Mathf.Pow(reachLerp, 1));
+            animator.SetFloat(ParameterName, LimbExtension);
 
-            //this.transform.position = Vector3.Lerp(startPosition, (player.transform.position), (Mathf.Pow(reachLerp, 2) * .6f));  
+            this.transform.position = Vector3.Lerp(startPosition, player.transform.position/reachDistance, (Mathf.Pow(reachLerp, LimbPowerFallOff)));  
         }
 
         //extension of blend tree and movement of this component 
     }
 
+
+    void AddTurbulence()
+    {
+        Vector3 position = transform.position * 100;
+
+        float time = Time.time * TurbulenceSpeed;
+
+        this.transform.position += new Vector3(Mathf.Sin(time + position.x), Mathf.Sin(time + position.y), Mathf.Sin(time + position.z)) * TurbulenceStrength;
+    }
 
 
 }
