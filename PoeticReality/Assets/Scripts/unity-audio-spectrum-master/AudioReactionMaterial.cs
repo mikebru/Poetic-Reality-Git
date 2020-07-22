4dbe00ee910d3704d6e38d692936389703597290
mikebru <32722658+mikebru@users.noreply.github.com>
2020-07-22 16:51:34 -0500

    working on shaders and music

---
+++ PoeticReality/Assets/Scripts/unity-audio-spectrum-master/AudioReactionMaterial.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CrazyMinnow.AmplitudeWebGL; // Import the AmplitudeWebGL namespace
+
+public class AudioReactionMaterial : MonoBehaviour
+{
+    public Material material;
+    public string ParameterName;
+
+    public enum BarType { Realtime, PeakLevel, MeanLevel };
+
+    public float Strength = 10;
+
+    public int index;
+    public BarType barType;
+
+    AudioSpectrumWebGL spectrum;
+
+    void Awake()
+    {
+        spectrum = FindObjectOfType(typeof(AudioSpectrumWebGL)) as AudioSpectrumWebGL;
+    }
+
+    void Update()
+    {
+        if (index < spectrum.Levels.Length)
+        {
+            float scale = 0.0f;
+
+            switch (barType)
+            {
+                case BarType.Realtime:
+                    scale = spectrum.Levels[index];
+                    break;
+                case BarType.PeakLevel:
+                    scale = spectrum.PeakLevels[index];
+                    break;
+                case BarType.MeanLevel:
+                    scale = spectrum.MeanLevels[index];
+                    break;
+            }
+
+            var vs = transform.localScale;
+            vs = Vector3.one * scale * Strength;
+            transform.localScale = vs;
+
+
+            material.SetFloat(ParameterName, vs.x);
+
+        }
+    }
+}
