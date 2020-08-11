9fc5033f6790c3f1db9130c6ef9f043e368e9c41
mikebru <32722658+mikebru@users.noreply.github.com>
2020-08-10 23:01:46 -0500

    working on flame shader, taking recordings

---
+++ PoeticReality/Assets/Scripts/MaterialTransitioner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialTransitioner : MonoBehaviour
+{
+    public Material[] materials;
+    public int index;
+    private Renderer[] renderers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+    }
+
+    private void OnValidate()
+    {
+        renderers = GetComponentsInChildren<Renderer>();
+        
+
+        ChangeMaterial();
+    }
+
+    void ChangeMaterial()
+    {
+        if (index <= materials.Length - 1)
+        {
+            foreach (Renderer renderer in renderers)
+            {
+                renderer.material = materials[index];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
